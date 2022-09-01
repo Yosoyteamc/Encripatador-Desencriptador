8ef2f540cffced81aa18125dff79d347dedb58f9
JUAN DANIEL MOSQUERA <juandaniel@unicauca.edu.co>
2022-09-01 14:29:20 -0500

    Pagina web de Encriptar y desencriptar, sprint challenge 01 Alura

---
+++ javascript/Control.js
@@ -0,0 +1,92 @@
+
+
+function encriptarTexto(){
+
+    var textoobtenido = document.getElementById("textoobtenido").value;
+    var campovacio = document.getElementById("textoresultado");
+    campovacio.style.backgroundImage = "none";
+
+    var texto = "";
+
+    for(var i=0; i<textoobtenido.length; i++){
+        if(textoobtenido[i]!="a" && textoobtenido[i]!="e" && textoobtenido[i]!="i"&& textoobtenido[i]!="o" && textoobtenido[i]!="u")
+        {texto = texto + textoobtenido[i];}
+        if(textoobtenido[i]=="e"){texto = texto + "enter";}
+        if(textoobtenido[i]=="i"){texto = texto + "imes";}
+        if(textoobtenido[i]=="a"){texto = texto + "ai";}
+        if(textoobtenido[i]=="o"){texto = texto + "ober";}
+        if(textoobtenido[i]=="u"){texto = texto + "ufat";}
+
+    }
+
+    document.getElementById('textoresultado').innerText = texto;
+
+}
+function desencriptarTexto(){
+
+    var textoobtenido = document.getElementById("textoobtenido").value;
+    var campovacio = document.getElementById("textoresultado");
+    campovacio.style.backgroundImage = "none";
+
+    var texto = "";
+
+    for(var i=0; i<textoobtenido.length; i++){
+        if(textoobtenido[i]=="a" && textoobtenido[i+1]=="i"){
+            texto = texto + "a";
+            i=i+2;
+        }
+        if(textoobtenido[i]=="i" && textoobtenido[i+1]=="m" && textoobtenido[i+2]=="e" && textoobtenido[i+3]=="s"){
+            texto = texto + "i";
+            i=i+4;
+        }
+        if(textoobtenido[i]=="o" && textoobtenido[i+1]=="b" && textoobtenido[i+2]=="e" && textoobtenido[i+3]=="r"){
+            texto = texto + "o";
+            i=i+4;
+        }
+        if(textoobtenido[i]=="u" && textoobtenido[i+1]=="f" && textoobtenido[i+2]=="a" && textoobtenido[i+3]=="t"){
+            texto = texto + "u";
+            i=i+4;
+        }
+        if(textoobtenido[i]=="e" && textoobtenido[i+1]=="n" && textoobtenido[i+2]=="t" && textoobtenido[i+3]=="e" && textoobtenido[i+4]=="r"){
+            texto = texto + "e";
+            i=i+5;
+        }
+
+        if(i>=textoobtenido.length){
+            break;
+        }
+
+        else {
+            texto = texto + textoobtenido[i];
+        }
+        
+        document.getElementById('textoresultado').innerText = texto;
+
+        
+
+    }
+
+    document.getElementById('textoresultado').innerText = texto;
+}
+
+function copiarTexto(){
+
+    let textoacopiar = document.querySelector("#textoresultado")
+    textoacopiar.select();
+    document.execCommand("copy");
+
+    document.getElementById('textoresultado').innerText = "";
+    document.getElementById('textoobtenido').value = "";
+
+    document.getElementById("textoobtenido").focus();
+
+    if (window.matchMedia("(min-width: 768px)").matches) {
+        var campovacio = document.getElementById("textoresultado");
+        campovacio.style.backgroundImage = "url('/Encriptador/images/Imagen.svg')";
+    } else {
+        return;
+    }
+   
+}
+
+
